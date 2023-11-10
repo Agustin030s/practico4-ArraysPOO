fdfdea9168038af089ada0765b503e09d34af3c6
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-10 15:42:45 -0300

    se realizo el ejercicio 3 de arrays

--- js/arrays/3-Ejercicio3.js
+++ js/arrays/3-Ejercicio3.js
@@ -2,4 +2,46 @@
 Catálogo de biblioteca: Tienes un array que almacena títulos de libros en una biblioteca. Debes crear funciones que permitan a los usuarios buscar libros por título, verificar si un libro específico está disponible para préstamo y llevar un registro de la disponibilidad de los libros.
 */
 
-const libros = [];
+const libros = ['POO en Java', 'PHP el lenguaje universal', 'Ingenieria de Software'];
+const disponibilidad = ['Disponible', 'No Disponible', 'No Disponible'];
+
+const mostrarLibros = (arreglo, titulo) => {
+    document.write(`<h2>Listado de libros ${titulo}</h2>`);
+    document.write("<ul>");
+    arreglo.map( item =>  document.write(`<li>${item}</li>`) );
+    document.write("</ul>");
+};
+
+const buscarLibros = titulo => {
+    const libroBuscado = libros.find(libro => libro.includes(titulo));
+    if(libroBuscado === undefined){
+        alert('No se encontro el libro que ingreso en la biblioteca');
+    }
+    return libroBuscado;
+}
+
+const verificarDisponibilidad = estado => estado === 'Disponible';
+const libroDisponible = titulo =>{
+    const libroIndex = libros.findIndex(item => item.includes(titulo));
+    const libro = buscarLibros(titulo);
+    if(libroIndex !== -1){
+        if(verificarDisponibilidad(disponibilidad[libroIndex])){
+            alert(`El libro ${libro} si esta disponible`);
+        } else{
+            alert(`El libro ${libro} no esta disponible`);
+        }
+    } else{
+        alert(`El libro ${libro} no se encontró en la biblioteca`);
+    }
+}
+
+const librosDisponibles = () =>{
+    const librosDisp = libros.filter((libro, index) => verificarDisponibilidad(disponibilidad[index]));
+    mostrarLibros(librosDisp, 'Disponibles');
+}
+
+const libroABuscar = prompt('Ingrese el titulo del libro que desea buscar');
+const libro = buscarLibros(libroABuscar);
+libroDisponible(libro);
+librosDisponibles();
+document.write(`<p>El libro es: ${libro}</p>`)
--- js/arrays/4-Ejercicio4.js
+++ js/arrays/4-Ejercicio4.js
@@ -2,67 +2,95 @@
 Gestión de proyectos en una empresa: Tienes dos arrays, uno para los nombres de proyectos y otro para los estados de los proyectos. Debes crear funciones que permitan agregar nuevos proyectos, cambiar el estado de un proyecto, asignar un responsable y generar un resumen de proyectos en curso.
 */
 
-const proyectos = ['Ecommerce', 'Microservicios con docker para sistema bancario', 'Aplicacion de ventas en .NET']
-const estadosProyecto = ['In progress', 'In progress', 'To Do'];
-const responsables = ['Luciano', 'Agustin', 'Sin Asignar']
+const proyectos = [
+  "Ecommerce",
+  "Microservicios con docker para sistema bancario",
+  "Aplicacion de ventas en .NET",
+];
+const estadosProyecto = ["In progress", "In progress", "To Do"];
+const responsables = ["Luciano", "Agustin", "Sin Asignar"];
 
 const mostrarArrays = (arreglo, titulo) => {
-    document.write(`<h2>Listado de ${titulo}</h2>`);
-    document.write("<ul>");
-    arreglo.map( item =>  document.write(`<li>${item}</li>`) );
-    document.write("</ul>");
+  document.write(`<h2>Listado de ${titulo}</h2>`);
+  document.write("<ul>");
+  arreglo.map((item) => document.write(`<li>${item}</li>`));
+  document.write("</ul>");
 };
 
 const agregarProyecto = (nombreProyecto) => {
-    proyectos.push(nombreProyecto);
-    estadosProyecto.push('To do');
-    responsables.push('Sin Asignar');
-}
+  proyectos.push(nombreProyecto);
+  estadosProyecto.push("To do");
+  responsables.push("Sin Asignar");
+};
 
 const cambiarEstadoDeProyecto = (nombreProyecto, estado) => {
-    const indexProyecto = proyectos.findIndex(proyecto => proyecto.includes(nombreProyecto));
-    const proyecto = proyectos.find(proyecto => proyecto.includes(nombreProyecto));
+  const indexProyecto = proyectos.findIndex((proyecto) =>
+    proyecto.includes(nombreProyecto)
+  );
+  const proyecto = proyectos.find((proyecto) =>
+    proyecto.includes(nombreProyecto)
+  );
 
-    if(indexProyecto !== -1){
-        estadosProyecto[indexProyecto] = estado;
-        return alert(`Al proyecto ${proyecto} se le cambio el estado a ${estado}`);
-    }else{
-        return alert(`No se puedo encontrar el proyecto ingresado`);
-    }
-}
+  if (indexProyecto !== -1) {
+    estadosProyecto[indexProyecto] = estado;
+    return alert(`Al proyecto ${proyecto} se le cambio el estado a ${estado}`);
+  } else {
+    return alert(`No se puedo encontrar el proyecto ingresado`);
+  }
+};
 
-const proyectoEnCurso = estado => estado === 'In progress';
+const proyectoEnCurso = (estado) => estado === "In progress";
 
 const asignarResponsableAlProyecto = (nombreProyecto, responsable) => {
-    const indexProyecto = proyectos.findIndex(proyecto => proyecto.includes(nombreProyecto));
-    const proyecto = proyectos.find(proyecto => proyecto.includes(nombreProyecto));
+  const indexProyecto = proyectos.findIndex((proyecto) =>
+    proyecto.includes(nombreProyecto)
+  );
+  const proyecto = proyectos.find((proyecto) =>
+    proyecto.includes(nombreProyecto)
+  );
 
-    if(indexProyecto !== -1){
-        if(responsables[indexProyecto] === 'Sin Asignar'){
-            responsables[indexProyecto] = responsable;
-            return alert(`Al proyecto ${proyecto} se le asigno el responsable ${responsable}`);
-        }else{
-            if(confirm(`El proyecto ${proyecto} ya tiene un responsable, desea cambiarselo?`)){
-                responsables[indexProyecto] = responsable;
-                return alert(`Al proyecto ${proyecto} se le asigno el responsable ${responsable}`);
-            }
-            return alert(`El proyecto ${proyecto} tiene como responsable a ${responsables[indexProyecto]}`);
-        }
-    }else{
-        return alert(`No se puedo encontrar el proyecto ingresado`);
+  if (indexProyecto !== -1) {
+    if (responsables[indexProyecto] === "Sin Asignar") {
+      responsables[indexProyecto] = responsable;
+      return alert(
+        `Al proyecto ${proyecto} se le asigno el responsable ${responsable}`
+      );
+    } else {
+      if (
+        confirm(
+          `El proyecto ${proyecto} ya tiene un responsable, desea cambiarselo?`
+        )
+      ) {
+        responsables[indexProyecto] = responsable;
+        return alert(
+          `Al proyecto ${proyecto} se le asigno el responsable ${responsable}`
+        );
+      }
+      return alert(
+        `El proyecto ${proyecto} tiene como responsable a ${responsables[indexProyecto]}`
+      );
     }
-}
+  } else {
+    return alert(`No se puedo encontrar el proyecto ingresado`);
+  }
+};
 
-const resumenProyectosEnCurso = () =>{
-    const proyectosEnCurso = proyectos.filter((proyecto, index) => proyectoEnCurso(estadosProyecto[index]));
-    mostrarArrays(proyectosEnCurso, 'Proyectos en curso');
-}
+const resumenProyectosEnCurso = () => {
+  const proyectosEnCurso = proyectos.filter((proyecto, index) =>
+    proyectoEnCurso(estadosProyecto[index])
+  );
+  mostrarArrays(proyectosEnCurso, "Proyectos en curso");
+};
 
-const nombreProyecto = prompt('Ingrese el proyecto al que desea cambiar el estado');
+const nombreProyecto = prompt(
+  "Ingrese el proyecto al que desea cambiar el estado"
+);
 // const estadoNuevo = prompt('Ingrese el nuevo estado del proyecto');
-const responsable = prompt('Ingrese el nombre del responsable que asignara al proyecto');
+const responsable = prompt(
+  "Ingrese el nombre del responsable que asignara al proyecto"
+);
 
 // agregarProyecto(nombreProyecto);
 // cambiarEstadoDeProyecto(nombreProyecto, estadoNuevo);
 asignarResponsableAlProyecto(nombreProyecto, responsable);
-resumenProyectosEnCurso()
+resumenProyectosEnCurso();
