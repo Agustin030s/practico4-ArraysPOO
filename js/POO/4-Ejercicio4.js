74ca6f754895c58b53802277ebc1ed7f9d6f55e4
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-15 09:39:11 -0300

    se realizo encapsulamiento y getter y setter en el ejercicio 4

--- js/POO/4-Ejercicio4.js
+++ js/POO/4-Ejercicio4.js
@@ -1,14 +1,41 @@
 class Producto {
+    #codigo
+    #nombre
+    #precio
     constructor(codigo, nombre, precio){
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.precio = precio;
+        this.#codigo = codigo;
+        this.#nombre = nombre;
+        this.#precio = precio;
     }
 
-    get imprimirDatos(){
-        document.write(`<h2>Código: ${this.codigo}</h2>`);
-        document.write(`<h2>Nombre: ${this.nombre}</h2>`);
-        document.write(`<h2>Precio: $ ${this.precio}</h2>`);
+    get codigo(){
+        return this.#codigo;
+    }
+
+    set codigo(codigoNuevo){
+        this.#codigo = codigoNuevo;
+    }
+
+    get nombre(){
+        return this.#nombre;
+    }
+
+    set nombre(nomNuevo){
+        this.#nombre = nomNuevo;
+    }
+
+    get precio(){
+        return this.#precio;
+    }
+
+    set precio(precioNuevo){
+        this.#precio = precioNuevo;
+    }
+
+    imprimirDatos(){
+        document.write(`<h2>Código: ${this.#codigo}</h2>`);
+        document.write(`<h2>Nombre: ${this.#nombre}</h2>`);
+        document.write(`<h2>Precio: $ ${this.#precio}</h2>`);
         document.write(`<br>`);
     }
 }
@@ -21,4 +48,4 @@ productos.push(producto2);
 const producto3 = new Producto(9, 'Arroz', 500);
 productos.push(producto3);
 
-productos.map(producto => producto.imprimirDatos);
+productos.map(producto => producto.imprimirDatos());
