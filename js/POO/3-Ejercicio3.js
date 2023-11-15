0257849ff9d86ff4d1ee1caf2f2d2362684103e0
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2023-11-15 09:44:59 -0300

    se realizo encapsulamiento y getter y setter en el ejercicio 3

--- js/POO/3-Ejercicio3.js
+++ js/POO/3-Ejercicio3.js
@@ -1,34 +1,47 @@
 class Rectangulo{
+    #alto;
+    #ancho;
     constructor(alto, ancho){
-        this.alto = alto,
-        this.ancho = ancho;
+        this.#alto = alto,
+        this.#ancho = ancho;
     }
 
-    get mostrarPropiedades(){
-        document.write(`<h2>Alto: ${this.alto}</h2>`);
-        document.write(`<h2>Ancho: ${this.ancho}</h2>`);
+    get alto(){
+        return this.#alto
     }
 
     set modificarAlto(altoNuevo){
-        this.alto = altoNuevo;
+        this.#alto = altoNuevo;
+    }
+
+    get ancho(){
+        return this.#ancho;
     }
 
     set modificarAncho(anchoNuevo){
-        this.ancho = anchoNuevo;
+        this.#ancho = anchoNuevo;
     }
 
-    get calcularPerimetro(){
-        return (2*this.alto) + (2*this.ancho);
+    calcularPerimetro(){
+        return (2*this.#alto) + (2*this.#ancho);
     }
 
-    get calcularArea(){
-        return this.alto*this.ancho;
+    calcularArea(){
+        return this.#alto*this.#ancho;
+    }
+
+    mostrarPropiedades(){
+        document.write(`<h2>Alto: ${this.alto} cm.</h2>`);
+        document.write(`<h2>Ancho: ${this.ancho} cm.</h2>`);
     }
 }
 
-const rect = new Rectangulo(10, 5);
-rect.mostrarPropiedades;
-const perimetro = rect.calcularPerimetro;
-const area = rect.calcularArea;
-console.log(perimetro);
-console.log(area);
+
+const alto = parseFloat(prompt('Ingrese el alto del rectangulo en cm.'));
+const ancho = parseFloat(prompt('Ingrese el ancho del rectangulo en cm.'));
+const rect = new Rectangulo(alto,ancho);
+rect.mostrarPropiedades();
+const perimetro = rect.calcularPerimetro();
+const area = rect.calcularArea();
+document.write(`<h2>El permitro del rectangulo es: ${perimetro} cm.</h2>`);
+document.write(`<h2>El area del rectangulo es: ${area} cm.</h2>`);
